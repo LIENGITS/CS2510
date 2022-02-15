c0a8d88b62e0a440723594caac04cf6f3b421929
GITLIEN <60283203+LIENGITS@users.noreply.github.com>
2022-02-15 16:13:42 -0500

    Update interleave.java

--- Function Method/interleave.java
+++ Function Method/interleave.java
@@ -22,3 +22,10 @@ public ILoString interleave(ILoString given) {
   public ILoString getRest() {
     return this.rest;
   }
+
+
+
+// list first: cons A B C D
+// List secoond: cons E F G H
+// return a new list that its 1,3,5 place is from "list first", then its 2,4,6 is from the" second list"
+// New list: cons A E B F C G D H 
