b6e5a4a30be645ec4a40b7f17ea3b401ce4524f3
GITLIEN <60283203+LIENGITS@users.noreply.github.com>
2022-02-15 00:50:38 -0500

    Create Sort the list in alphabetical order & lower case.java

---
+++ Function Method/Sort the list in alphabetical order & lower case.java
@@ -0,0 +1,35 @@
+class MtLoString implements ILoString {
+public ILoString sort() {
+    return this;
+  }
+  
+  public ILoString toLowerCase() {
+    return this;
+  }
+  
+   public ILoString insert(String given) {
+    return new ConsLoString(given, this);
+  }
+  }
+  
+  class ConsLoString implements ILoString {
+  
+    public ILoString sort() {
+    return this.rest.sort().insert(this.first.toLowerCase());
+
+  }
+
+  // to lowerCase
+  public ILoString toLowerCase() {
+    return new ConsLoString(this.first.toLowerCase(), this.rest.toLowerCase());
+  }
+
+  // insert the given string into the list
+  public ILoString insert(String given) {
+    if (given.compareTo(this.first) <= 0) {
+      return new ConsLoString(given, this);
+    }
+    else {
+      return new ConsLoString(this.first, this.rest.insert(given));
+    }
+  }
